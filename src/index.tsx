2344761d644d71cb8cd77b7bb25987c37fa3cec3
Halanau327 <a.halanau@gmail.com>
2024-06-10 23:46:57 +0300

    refactor: add new component 'Task'

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import AppWithRedux from "./AppWithRedux";
-import {store} from "./module/store";
-import {Provider} from "react-redux";
-import AppWithReducers from "./AppWithReducers";
+const course = {
+    name: "Maxim",
+    age: 48,
+    technologies: ["HTML", "", "REACT"]
+}
 
+const [board = "FOO", respect = "BOO", heart = "DOO"] = course.technologies
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-    <Provider store={store}>
-        <AppWithRedux/>
-     {/*<App/>*/}
-     {/*   <AppWithReducers/>*/}
-    </Provider>
-);
+if(!board){
+    alert("HEY")
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Какую переменную следует указать вместо XXX, что бы была вызвана функция alert?
