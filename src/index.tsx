3d8c06022e9075a7d56986f0f5f140e8a067cd83
Halanau327 <a.halanau@gmail.com>
2024-06-12 18:26:54 +0300

    update: add StoryBook

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
-const course = {
-    name: "Maxim",
-    age: 48,
-    technologies: ["HTML", "", "REACT"]
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import AppWithRedux from "./AppWithRedux";
+import {store} from "./module/store";
+import {Provider} from "react-redux";
+import AppWithReducers from "./AppWithReducers";
 
-const [board = "FOO", respect = "BOO", heart = "DOO"] = course.technologies
 
-if(!board){
-    alert("HEY")
-}
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+    <Provider store={store}>
+        <AppWithRedux/>
+     {/*<App/>*/}
+     {/*   <AppWithReducers/>*/}
+    </Provider>
+);
 
-// Какую переменную следует указать вместо XXX, что бы была вызвана функция alert?
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
